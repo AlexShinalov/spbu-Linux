6ff87a6d3a0b5ea797a1d30c1b4e816b3514b799
v131v <jsjsjs87@mail.ru>
2022-11-18 01:36:42 +0000

    feat(7.2): add 7.2 bash script

---
+++ scripts/7 conspect/2.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd ./repo
+git checkout -b feature
+
+cat >newFile.txt<<<"New text"
+
+git add .
+git commit -m "feat: add new file"
+
+git checkout main
+git merge feature
+git branch -D feature
+
+git log --oneline
+
