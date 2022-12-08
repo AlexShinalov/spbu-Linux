f4bcda9f00c44d4f003039bc5728ac57aad38e59
The President <116872285+finstape@users.noreply.github.com>
2022-12-08 17:42:07 +0300

    Create 3.cpp

---
+++ scripts/12 conspect/3.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+        unsigned int a = UINT_MAX;
+        cout << "a=" <<  a << '\n';
+        int aa = static_cast<int>(a);
+        cout << "aa=" << aa << '\n';
+        int b = INT_MAX;
+        cout << "b=" << b << '\n';
+        unsigned int bb = static_cast<unsigned int>(b);
+        cout << "bb=" << bb << '\n';
+        return 0;
+}
